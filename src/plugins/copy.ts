cb54e56ad6cad1cf595b20747ba1ebf95339ed1b
cody <cevek@ya.ru>
2016-11-16 17:41:57 +0500

    Add pathModificator callback to copy

--- src/plugins/copy.ts
+++ src/plugins/copy.ts
@@ -1,13 +1,18 @@
 import {plugin} from "../packer";
 import {Glob} from "../utils/fs";
 import {Plugin} from "../utils/Plugin";
+import * as path from 'path';
 
-export function copy(globFiles: Glob) {
+export function copy(globFiles: Glob, pathModificator?: (filename: string) => string) {
     return plugin('copy', async(plug: Plugin) => {
         const files = await plug.fs.findFiles(globFiles);
         for (let i = 0; i < files.length; i++) {
             const file = files[i];
-            const destFileName = plug.normalizeDestName(file.fullName);
+            let relativeName = path.relative(plug.options.context, file.fullName);
+            if (pathModificator) {
+                relativeName = pathModificator(relativeName);
+            }
+            const destFileName = plug.normalizeDestName(relativeName);
             const destFile = await plug.fs.createGeneratedFromFile(destFileName, file);
             plug.stage.addFile(destFile);
         }
