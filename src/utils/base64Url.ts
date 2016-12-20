b622456b4f48c75fcfe1daafaab5a334494d5dbb
cody <cevek@ya.ru>
2016-12-20 20:30:05 +0500

    Add base64Url

---
+++ src/utils/base64Url.ts
@@ -0,0 +1,16 @@
+const mime = require('mime-db');
+const extensions: {[ext: string]: string} = {};
+const keys = Object.keys(mime);
+for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const exts = mime[key].extensions;
+    if (exts) {
+        for (let j = 0; j < exts.length; j++) {
+            const ext = exts[j];
+            extensions[ext] = key;
+        }
+    }
+}
+export function base64Url(ext: string, body: string) {
+    return `data:${extensions[ext] || 'application/octet-stream'};base64,${new Buffer(body).toString('base64')}`;
+}
