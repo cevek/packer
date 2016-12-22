439434f0e317263cfac6708c9a43c4fc95213e6b
cevek <cevek@ya.ru>
2016-12-22 16:58:53 +0300

    Add makeHashBinary

--- src/utils/makeHash.ts
+++ src/utils/makeHash.ts
@@ -10,3 +10,15 @@ export function makeHash(str: string) {
      * signed int to an unsigned by doing an unsigned bitshift. */
     return hash >>> 0;
 }
+
+export function makeHashBinary(buff: Buffer) {
+    let hash = 5381;
+    let i = buff.length;
+    while (i) {
+        hash = (hash * 33) ^ buff[--i];
+    }
+    /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
+     * integers. Since we want the results to be always positive, convert the
+     * signed int to an unsigned by doing an unsigned bitshift. */
+    return hash >>> 0;
+}
