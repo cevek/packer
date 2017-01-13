0a38a71087c608b1d27172198c27135117e08a41
cevek <cevek@ya.ru>
2017-01-13 15:55:38 +0300

    Refactor html plugin to use ejs

--- src/plugins/html.ts
+++ src/plugins/html.ts
@@ -5,18 +5,19 @@ import {logger} from "../utils/logger";
 import {SourceFile} from "../utils/SourceFile";
 import {makeHash, makeHashBinary} from "../utils/makeHash";
 import {relative} from "path";
+const ejs = require('ejs');
 
 import path = require('path');
 
 export interface HTMLRenderData {
-    htmlFileContent: string;
+    template: (p: HTMLReplaceParams) => string;
     params: HTMLReplaceParams;
-    urlReplacedParams: HTMLReplaceParams;
 }
-const pluginMap = new Map<Plugin, HTMLRenderData>();
+
+const pluginMap = new Map<Plugin, Map<string, HTMLRenderData>>();
 
 export interface HTMLReplaceParams {
-    [key: string]: string | number;
+    [key: string]: any;
 }
 export interface HTMLOptions {
     file: string,
@@ -31,6 +32,17 @@ interface HTMLCache {
 export function html(options: HTMLOptions) {
     return plugin('html', async(plug: Plugin) => {
         const files = await plug.fs.findFiles(options.file);
+        const cache = plug.getCache('html') as HTMLCache;
+        if (!cache.urlData) {
+            cache.urlData = new Map();
+        }
+
+        let renderDataItems = pluginMap.get(plug);
+        if (!renderDataItems) {
+            renderDataItems = new Map();
+            pluginMap.set(plug, renderDataItems);
+        }
+
         files.forEach(file => {
             plug.stage.addFile(file);
             plug.fs.watch(file);
@@ -46,7 +58,15 @@ export function html(options: HTMLOptions) {
         const updatedFiles = files.filter(file => file.updated);
         for (let i = 0; i < updatedFiles.length; i++) {
             const file = updatedFiles[i];
-            const newSource = await replace(file, options.params, plug);
+            let newSource = await replaceHref(file, plug, cache);
+            const params = preparePredefinedParams(options.params, plug);
+            const renderData: HTMLRenderData = {
+                params: params,
+                template: null
+            };
+            renderDataItems.set(file.fullName, renderData);
+            renderData.template = ejs.compile(newSource, {compileDebug: true});
+            newSource = renderData.template(params);
             const destFilename = plug.normalizeDestName(options.destFile);
             const destFile = plug.fs.createGeneratedFile(destFilename, newSource, file);
             destFile.nameCanBeHashed = false;
@@ -61,119 +81,102 @@ export function html(options: HTMLOptions) {
     });
 }
 
+function preparePredefinedParams(params: HTMLReplaceParams, plug: Plugin, packerResult?: PackerResult) {
+    let cssFiles: string[];
+    let jsFiles: string[];
+    if (packerResult) {
+        cssFiles = packerResult.emittedCSSFiles;
+        jsFiles = packerResult.emittedJSFiles;
+    } else {
+        const stage = plug.stage.list();
+        cssFiles = [];
+        jsFiles = [];
+        for (let i = 0; i < stage.length; i++) {
+            const file = stage[i];
+            if (file.extName == 'js' || file.extName == 'css' && plug.inDestFolder(file)) {
+                const src = plug.options.publicPath + plug.relativeToDest(file);
+                if (file.extName == 'js') {
+                    jsFiles.push(src);
+                }
+                else {
+                    cssFiles.push(src);
+                }
+            }
+        }
+    }
 
-export function renderHTML(packerResult: PackerResult, overrideParams: HTMLReplaceParams) {
-    const cssFiles = packerResult.emittedCSSFiles;
-    const jsFiles = packerResult.emittedJSFiles;
-    const plug = packerResult.plugin;
-    const renderData = pluginMap.get(plug);
-    const params = Object.assign(renderData.params, overrideParams);
-    return renderData.htmlFileContent.replace(/%(.*?)%/g, (m: string, m1: string) => {
-        let urlM: RegExpMatchArray;
-        if (urlM = m1.match(/url\(\s*['"]?(.*?)['"]?\s*\)/i)) {
-            const url = urlM[1];
-            return renderData.urlReplacedParams[url] as string;
-        } else if (m1 === 'js') {
-            let out = '\n';
-            for (let i = 0; i < jsFiles.length; i++) {
-                const filename = jsFiles[i];
-                const src = plug.options.publicPath + relative(plug.options.dest, filename);
-                out += `\t<script src="${src}"></script>\n`;
-            }
-            return out;
-        } else if (m1 === 'css') {
-            let out = '\n';
-            for (let i = 0; i < cssFiles.length; i++) {
-                const filename = cssFiles[i];
-                const src = plug.options.publicPath + relative(plug.options.dest, filename);
-                out += `\t<link rel="stylesheet" href="${src}">\n`;
-            }
-            return out;
-        }
-        let out = params[m1] as string;
-        if (out == null) {
-            logger.warning(`html plugin: ${m1} param is ${out}`);
-            out = '';
-        }
-        return out;
-    });
+    let js = '\n';
+    for (let i = 0; i < jsFiles.length; i++) {
+        const filename = jsFiles[i];
+        const src = plug.options.publicPath + relative(plug.options.dest, filename);
+        js += `\t<script src="${src}"></script>\n`;
+    }
+
+    let css = '\n';
+    for (let i = 0; i < cssFiles.length; i++) {
+        const filename = cssFiles[i];
+        const src = plug.options.publicPath + relative(plug.options.dest, filename);
+        css += `\t<link rel="stylesheet" href="${src}">\n`;
+    }
+
+    params['js'] = js;
+    params['css'] = css;
+    return params;
 }
 
-async function replace(htmlFile: SourceFile, replaceParams: HTMLReplaceParams, plug: Plugin) {
-    const source = await plug.fs.readContent(htmlFile);
-    const cache = plug.getCache('html') as HTMLCache;
-    if (!cache.urlData) {
-        cache.urlData = new Map();
+export function renderHTML(filename: string, packerResult: PackerResult, overrideParams: HTMLReplaceParams) {
+    const plug = packerResult.plugin;
+    filename = plug.normalizeName(filename);
+    const renderDataMap = pluginMap.get(plug);
+    if (!renderDataMap) {
+        throw new Error('No assigned html data for current results');
     }
-    const renderData: HTMLRenderData = {
-        params: replaceParams,
-        urlReplacedParams: {},
-        htmlFileContent: source,
-    };
-    pluginMap.set(plug, renderData);
-    const stage = plug.stage.list();
-    return source.replace(/%(.*?)%/g, (m: string, m1: string) => {
-        let urlM: RegExpMatchArray;
-        if (urlM = m1.match(/url\(\s*['"]?(.*?)['"]?\s*\)/i)) {
-            const url = urlM[1];
-            const filename = plug.normalizeName(url);
-            const urlFile = plug.fs.tryFileSync(filename);
-            if (!urlFile) {
-                logger.warning(`Cannot find ${filename} in ${htmlFile.fullName}`);
-            }
-            let newUrl = cache.urlData.get(urlFile);
-            if (!newUrl || urlFile.updated) {
-                plug.fs.readContentSync(urlFile);
-                const urlBinaryContent = urlFile.content;
-                const urlBasename = (makeHash(urlFile.fullName) + makeHashBinary(urlBinaryContent)).toString(36) + '.' + urlFile.extName;
-                const urlDestFileName = plug.normalizeDestName(urlBasename);
-                const urlDestFile = plug.fs.createGeneratedFile(urlDestFileName, urlBinaryContent, htmlFile);
-                plug.stage.addFile(urlDestFile);
-                urlDestFile.nameCanBeHashed = false;
-                if (!htmlFile.imports) {
-                    htmlFile.imports = [];
-                }
-                htmlFile.imports.push({
-                    module: url,
-                    file: urlFile,
-                    startLine: null,
-                    startColumn: null,
-                    endLine: null,
-                    endColumn: null,
-                    startPos: null,
-                    endPos: null,
-                });
-                newUrl = plug.options.publicPath + plug.relativeToDest(urlDestFile);
-                cache.urlData.set(urlFile, newUrl);
-                renderData.urlReplacedParams[url] = newUrl;
-            }
-            return newUrl;
-        } else if (m1 === 'js') {
-            let out = '\n';
-            for (let i = 0; i < stage.length; i++) {
-                const file = stage[i];
-                if (file.extName == 'js' && plug.inDestFolder(file)) {
-                    const src = plug.options.publicPath + plug.relativeToDest(file);
-                    out += `\t<script src="${src}"></script>\n`;
-                }
-            }
-            return out;
-        } else if (m1 === 'css') {
-            let out = '\n';
-            for (let i = 0; i < stage.length; i++) {
-                const file = stage[i];
-                if (file.extName == 'css' && plug.inDestFolder(file)) {
-                    const src = plug.options.publicPath + plug.relativeToDest(file);
-                    out += `\t<link rel="stylesheet" href="${src}">\n`;
-                }
-            }
-            return out;
+    const renderData = renderDataMap.get(filename);
+    if (!renderData) {
+        throw new Error('No assigned html data for: ' + filename);
+    }
+    const params = preparePredefinedParams(Object.assign(renderData.params, overrideParams), plug, packerResult);
+    return renderData.template(params);
+}
+
+async function replaceHref(htmlFile: SourceFile, plug: Plugin, cache: HTMLCache) {
+    const source = await plug.fs.readContent(htmlFile);
+    return source.replace(/ href=["']\s*(.*?)\s*["']/ig, (m: string, m1: string) => {
+        const url = m1;
+        if (/^https?:/i.test(m1)) {
+            return m;
         }
-        let out = replaceParams[m1] as string;
-        if (out == null) {
-            logger.warning(`html plugin: ${m1} param is ${out}`);
-            out = '';
+        const filename = plug.normalizeName(url);
+        const urlFile = plug.fs.tryFileSync(filename);
+        if (!urlFile) {
+            logger.warning(`Cannot find ${filename} in ${htmlFile.fullName}`);
+            return m;
         }
-        return out;
+        let newUrl = cache.urlData.get(urlFile);
+        if (!newUrl || urlFile.updated) {
+            plug.fs.readContentSync(urlFile);
+            const urlBinaryContent = urlFile.content;
+            const urlBasename = (makeHash(urlFile.fullName) + makeHashBinary(urlBinaryContent)).toString(36) + '.' + urlFile.extName;
+            const urlDestFileName = plug.normalizeDestName(urlBasename);
+            const urlDestFile = plug.fs.createGeneratedFile(urlDestFileName, urlBinaryContent, htmlFile);
+            plug.stage.addFile(urlDestFile);
+            urlDestFile.nameCanBeHashed = false;
+            if (!htmlFile.imports) {
+                htmlFile.imports = [];
+            }
+            htmlFile.imports.push({
+                module: url,
+                file: urlFile,
+                startLine: null,
+                startColumn: null,
+                endLine: null,
+                endColumn: null,
+                startPos: null,
+                endPos: null,
+            });
+            newUrl = plug.options.publicPath + plug.relativeToDest(urlDestFile);
+            cache.urlData.set(urlFile, newUrl);
+        }
+        return ' href="' + newUrl + '"';
     });
 }
